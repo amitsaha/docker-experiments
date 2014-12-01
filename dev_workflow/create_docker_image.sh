3eb6d7f23059520618e5bb47a5436b6b696fd104
Amit Saha <amitsaha.in@gmail.com>
2014-12-01 16:23:36 +1000

    Dev workflow: create a docker image with the default user as the current user

---
+++ dev_workflow/create_docker_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ./create_docker_image.sh <image_name> <docker image>
+uid=`id -u`
+user=`id -un`
+
+cat <<EOF > Dockerfile
+
+FROM $2
+
+RUN useradd -u $uid $user
+USER $uid
+
+EOF
+
+docker build -t $1 .
+mv Dockerfile Dockerfile.$1
