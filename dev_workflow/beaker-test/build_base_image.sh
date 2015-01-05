de0520adafea4f1cdec16357441b67b922cd35b2
Amit Saha <amitsaha.in@gmail.com>
2015-01-05 14:05:40 +1000

    bad ENV command

--- dev_workflow/beaker-test/build_base_image.sh
+++ dev_workflow/beaker-test/build_base_image.sh
@@ -2,7 +2,6 @@
 
 cat <<EOF > Dockerfile
 FROM fedora:21
-ENV SECONDS=`date +%s`
 RUN yum -y install wget yum-utils createrepo createrepo_c git python-unittest2 mariadb-server mariadb openldap-servers python-pip
 RUN pip install --upgrade selenium
 ADD install_deps.sh /root/install_deps.sh
