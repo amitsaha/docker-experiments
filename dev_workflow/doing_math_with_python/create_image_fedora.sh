95f7bd4a87278f5844459d1740d50aab429ace65
Amit Saha <amitsaha.in@gmail.com>
2015-02-21 23:58:19 +1000

    update dmwp dockerfile and linking demo

--- dev_workflow/doing_math_with_python/create_image_fedora.sh
+++ dev_workflow/doing_math_with_python/create_image_fedora.sh
@@ -5,8 +5,8 @@ user=`id -un`
 cat <<EOF > Dockerfile
 
 FROM fedora:$1
-RUN yum -y install python3-pip gcc gcc-c++ @base-x openssh-server python3-matplotlib python3-matplotlib-tk python3-sympy python3-scipy python3-numpy python3-mock python3-nose
-RUN pip3 install matplotlib-venn
+RUN yum -y install python3-pip gcc gcc-c++ @base-x openssh-server python3-matplotlib python3-matplotlib-tk python3-sympy python3-scipy python3-numpy python3-mock python3-nose python3-ipython
+RUN pip-python3 install matplotlib-venn
 
 # password: password
 RUN useradd --groups wheel -m -d /home/$user -p aajfMKNH1hTm2 $user
@@ -19,3 +19,4 @@ ENTRYPOINT ["/usr/sbin/sshd", "-D"]
 EOF
 
 docker build -t fedora$1_dmwp .
+rm Dockerfile
