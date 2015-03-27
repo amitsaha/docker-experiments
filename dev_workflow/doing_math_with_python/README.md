97e9f7c8eb788739aab6bc5d1e4f1d2a6304f6ee
Amit Saha <amitsaha.in@gmail.com>
2015-03-27 16:20:52 +1000

    amitsaha/dmwp: no ssh needed

---
+++ dev_workflow/doing_math_with_python/README.md
@@ -0,0 +1,3 @@
+```
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -ti amitsaha/fedora20_dmwp bash
+```
--- dev_workflow/doing_math_with_python/create_image_fedora_20.sh
+++ dev_workflow/doing_math_with_python/create_image_fedora_20.sh
@@ -11,12 +11,8 @@ RUN pip-python3 install matplotlib-venn==0.11 sympy==0.7.6
 
 # password: password
 RUN useradd -u $uid $user -d /home/$user -p aajfMKNH1hTm2
-
-# Most likely best reference: https://bugzilla.redhat.com/show_bug.cgi?id=966807
-RUN /usr/bin/sed -e '/session    required     pam_loginuid.so/ s/^#*/#/' -i /etc/pam.d/sshd
-EXPOSE 22
-RUN /usr/sbin/sshd-keygen
-ENTRYPOINT ["/usr/sbin/sshd", "-D"]
+USER $user
+CMD bash
 EOF
 
 docker build -t amitsaha/fedora20_dmwp .
